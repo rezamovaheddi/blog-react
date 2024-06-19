b170488f6956307a2c267b5fb60af45155a2fd62
rezamovaheddi <rmwhdy8@gamil.com>
2024-06-19 17:37:46 +0330

    creating react project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+// import User from './components/users/User'
+import Navbar from './components/navbar/Navbar'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+
+function App() {
+  return (
+    <Navbar />
+  )
+}
+
+export default App
+
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+function Navbar() {
+    return (
+        <div>
+           <ul className="header">
+            <li>لیست مقالات</li>
+            <li>مقاله جدید</li>
+            <li>درباره ما</li>
+           </ul>
+        </div>
+    )
+}
+export default Navbar
